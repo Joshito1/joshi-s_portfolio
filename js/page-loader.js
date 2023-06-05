6ba2389a6a547b23435f22acce4c27532eaf0aca
Joshito1 <marleyaguilera31@gmail.com>
2023-06-04 21:55:51 -0400

    fixed: loader not loading

--- js/page-loader.js
+++ js/page-loader.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
     var loader = $('#loader');
     var loaderBar = $('#loader-bar');
     var loaderTxt = $('#loader-txt');
-    var content = $('.content');
+    var content = $('#content');
 
     $('body').append(loaderContainer);
 
@@ -108,7 +108,6 @@ $(document).ready(function () {
 
     checkResources();
 });
-
 
 
 
