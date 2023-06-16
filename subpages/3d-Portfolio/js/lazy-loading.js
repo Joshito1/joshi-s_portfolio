c8eb3abb55f601d0287743aa10839a12f0688234
Joshito1 <marleyaguilera31@gmail.com>
2023-06-15 21:46:50 -0400

    Fixed: Lazy loading for the blur to wait until images load

--- subpages/3d-Portfolio/js/lazy-loading.js
+++ subpages/3d-Portfolio/js/lazy-loading.js
@@ -1,3 +1,25 @@
+// const lazyLoading = () => {
+//     const lazyImgs = document.querySelectorAll(".lazy");
+
+//     const observer = new IntersectionObserver((entries, observer) => {
+//         entries.forEach((entry) => {
+//             if (entry.isIntersecting) {
+//                 let img = entry.target;
+//                 img.src = img.dataset.src;
+//                 img.classList.remove('loading');
+//                 img.classList.add('loaded');
+//                 observer.unobserve(img);
+//             }
+//         });
+//     });
+
+//     lazyImgs.forEach((img) => {
+//         observer.observe(img);
+//     });
+// };
+
+// lazyLoading();
+
 const lazyLoading = () => {
     const lazyImgs = document.querySelectorAll(".lazy");
 
@@ -6,8 +28,16 @@ const lazyLoading = () => {
             if (entry.isIntersecting) {
                 let img = entry.target;
                 img.src = img.dataset.src;
-                img.classList.remove('loading');
-                img.classList.add('loaded');
+
+                // Apply blur effect
+                img.classList.add('loading');
+
+                img.onload = function () {
+                    // Remove blur effect after image has loaded
+                    img.classList.remove('loading');
+                    img.classList.add('loaded');
+                };
+
                 observer.unobserve(img);
             }
         });
