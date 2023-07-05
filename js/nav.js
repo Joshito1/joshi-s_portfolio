c34654258ce4fd1df5099c603ed7d60a903e5587
Joshito1 <marleyaguilera31@gmail.com>
2023-07-04 22:01:33 -0400

    Modified: Universal Navigation Gui. Reports.html Lonely message. Servers.html message.
    Deleted:
    About.css File.
    
    Fixed:
    Actually Fixed the loader load this time.
    Height of ruckers Mobile Link menu to fit browser

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 // Hamburger Menu //
 ///////////////////
-const universal_hamburger = document.querySelector('#nav-toggle');
-const universal_menu = document.querySelector('#nav');
+const universal_hamburger = document.querySelector('#univ_nav_button');
+const universal_menu = document.querySelector('#universal_nav');
 
 universal_hamburger.addEventListener('click', () => {
     universal_hamburger.classList.toggle('active');
