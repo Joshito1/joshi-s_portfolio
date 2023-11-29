d979bb06a8bcae7c4db1bd4dae2448ddd86f1c1c
Joshito <marleyaguilera31@gmail.com>
2023-11-29 02:31:33 -0500

    Added: - Scroll enabled messege to universal nav menu and script tag. - Faded background to Univiversal nav menu background and to the nav.js.
    Modified:
    - Universal Nav Menu Styles.
    - Z-indexes and removed unnecessary z-index.
    - Debug button color to transparent.
    - Ruckers gift card info repeation.
    
    To Do:
    - Fix 3D Gallery render modal hieght issue.
    - Modify Scroll messege,add timer to dissapear after certain time, add scrolling disabled.
    - Move Scrolling message script tag to js file.
    - Connect Links for universal nav.
    - Update universal nav menu to all necessary pages.

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,11 @@
 // Hamburger Menu //
 ///////////////////
 const universal_hamburger = document.querySelector('#univ_nav_button');
+const universal_menu_bg = document.querySelector('#universal_nav_bg');
 const universal_menu = document.querySelector('#universal_nav');
 
 universal_hamburger.addEventListener('click', () => {
     universal_hamburger.classList.toggle('active');
     universal_menu.classList.toggle('active');
+    universal_menu_bg.classList.toggle('active')
 });
