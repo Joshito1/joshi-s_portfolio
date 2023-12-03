78c81e1618e512caa5b88069879c7567801a47d6
Joshito <marleyaguilera31@gmail.com>
2023-12-02 21:18:26 -0500

    ruckers-nav-change - change log #1
    Modified:
    - Ruckers nav to have exterior navigational buttons.
    - Ruckers Footer to be separate from main content.
    - Ruckers universal.css styles.
    
    Deleted:
    - Missed duplicate universal nav button in ruckers.
    
    To Do:
    - Fix 3D Gallery render modal height issue.
    - Make an ambient ring to the renders in 3D Gallery Modal.

--- subpages/web_dev-Portfolio/ruckers-website/js/universal.js
+++ subpages/web_dev-Portfolio/ruckers-website/js/universal.js
@@ -1,9 +1,11 @@
 // Hamburger Menu //
 ///////////////////
+const exteriorLinks = document.querySelector('.exterior-menu-links');
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.burger-menu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   menu.classList.toggle('active');
+  exteriorLinks.classList.toggle('fade');
 });
